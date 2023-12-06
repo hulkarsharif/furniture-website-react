a183e651be8f9ddce11c4757ca39a0aec9ed9af9
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-12-05 22:16:53 -0500

    Finihs the process react

--- src/pages/home/sections/process/Process.js
+++ src/pages/home/sections/process/Process.js
@@ -1,81 +1,118 @@
 import "./Process.css";
+import { processes } from "./data";
 
+const ProcessRow = (props) => {
+    return (
+        <div className="process-line">
+            <div className="number">
+                <p className="num">{props.order}</p>
+            </div>
+            <div className="process-heading">
+                <h4 className="heading-4">{props.title}</h4>
+            </div>
+            <div className="process-text">
+                <p className="text-lg">{props.description}</p>
+            </div>
+            <div className="process-link">
+                <div className="orange-line"></div>
+                <a className="uppercase-text-2" href={props.link}>
+                    {" "}
+                    LEARN MORE
+                </a>
+            </div>
+        </div>
+    );
+};
 export function Process() {
     return (
         <section id="process-section">
-            <div class="process-section container">
-                <h5 class="uppercase-text-1">OUR WORKING PROCESS</h5>
-
-                <div class="process-line">
-                    <div class="number">
-                        <p class="num">01.</p>
+            <div className="process-section container">
+                <h5 className="uppercase-text-1">OUR WORKING PROCESS</h5>
+                {processes.map((process, idx) => {
+                    return (
+                        <ProcessRow
+                            key={idx}
+                            order={process.order}
+                            title={process.title}
+                            description={process.description}
+                            link={process.link}
+                        />
+                    );
+                })}
+                {/* <div className="process-line">
+                    <div className="number">
+                        <p className="num">01.</p>
                     </div>
-                    <div class="process-heading">
-                        <h4 class="heading-4">Discuss the concept with us.</h4>
+                    <div className="process-heading">
+                        <h4 className="heading-4">
+                            Discuss the concept with us.
+                        </h4>
                     </div>
-                    <div class="process-text">
-                        <p class="text-lg">
+                    <div className="process-text">
+                        <p className="text-lg">
                             Engage in an insightful conversation with our team
                             to explore your project's vision, goals, and desired
                             outcomes
                         </p>
                     </div>
-                    <div class="process-link">
-                        <div class="orange-line"></div>
-                        <a class="uppercase-text-2" href="">
+                    <div className="process-link">
+                        <div className="orange-line"></div>
+                        <a className="uppercase-text-2" href="">
                             {" "}
                             LEARN MORE
                         </a>
                     </div>
                 </div>
 
-                <div class="process-line">
-                    <div class="number">
-                        <p class="num">02.</p>
+                <div className="process-line">
+                    <div className="number">
+                        <p className="num">02.</p>
                     </div>
-                    <div class="process-heading">
-                        <h4 class="heading-4">Confirm the provided design.</h4>
+                    <div className="process-heading">
+                        <h4 className="heading-4">
+                            Confirm the provided design.
+                        </h4>
                     </div>
-                    <div class="process-text">
-                        <p class="text-lg">
+                    <div className="process-text">
+                        <p className="text-lg">
                             Once you provide us with the design you envision,
                             our experienced team will meticulously review and
                             validate every aspect.
                         </p>
                     </div>
-                    <div class="process-link">
-                        <div class="orange-line"></div>
-                        <a class="uppercase-text-2" href="">
+                    <div className="process-link">
+                        <div className="orange-line"></div>
+                        <a className="uppercase-text-2" href="">
                             {" "}
                             LEARN MORE
                         </a>
                     </div>
                 </div>
 
-                <div class="process-line">
-                    <div class="number">
-                        <p class="num">03.</p>
+                <div className="process-line">
+                    <div className="number">
+                        <p className="num">03.</p>
                     </div>
-                    <div class="process-heading">
-                        <h4 class="heading-4">
+                    <div className="process-heading">
+                        <h4 className="heading-4">
                             Get delivered the furniture on time.
                         </h4>
                     </div>
-                    <div class="process-text">
-                        <p class="text-lg">
+                    <div className="process-text">
+                        <p className="text-lg">
                             Our dedicated team prioritizes punctuality and
                             efficient logistics to ensure that your furniture
                             arrives precisely.
                         </p>
                     </div>
-                    <div class="process-link last">
-                        <div class="orange-line"></div>
-                        <a class="uppercase-text-2" href="">
+                    <div className="process-link last">
+                        <div className="orange-line"></div>
+                        <a className="uppercase-text-2" href="">
                             {" "}
                             LEARN MORE
                         </a>
                     </div>
-                </div>
+                </div> */}
             </div>
         </section>
     );
---
+++ src/pages/home/sections/process/data.js
@@ -0,0 +1,24 @@
+import "./Process.css";
+export const processes = [
+    {
+        order: "01",
+        title: "Discuss the concept with us",
+        description:
+            "Engage in an insightful conversation with our team to explore your project's vision, goals, and desired outcomes",
+        link: ""
+    },
+    {
+        order: "02",
+        title: "Discuss the Confirm the provided design.",
+        description:
+            "Once you provide us with the design you envision,our experienced team will meticulously review and validate every aspect.",
+        link: ""
+    },
+    {
+        order: "03",
+        title: "Get delivered the furniture on time.",
+        description:
+            "Our dedicated team prioritizes punctuality and efficient logistics to ensure that your furniture arrives precisely.",
+        link: ""
+    }
+];
