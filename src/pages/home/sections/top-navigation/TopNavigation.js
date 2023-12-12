1657c10c7fccfbaa4ca1ed6d953b0e43523f7c1e
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-12-12 14:33:46 -0500

    refactor top nav with state

--- src/pages/home/sections/top-navigation/TopNavigation.js
+++ src/pages/home/sections/top-navigation/TopNavigation.js
@@ -1,5 +1,7 @@
+import React from "react";
 import "./TopNavigation.css";
 import logo from "../../../../images/logo-white.jpg";
+import burgerIcon from "../../../../images/menu 1.jpg";
 
 const links = [
     { text: "Products", link: "https://google.com" },
@@ -7,38 +9,75 @@ const links = [
     { text: "Inspiration", link: "" },
     { text: "Support", link: "" }
 ];
-export function TopNavigation() {
-    return (
-        <header>
-            <nav className="navigation container">
-                <img className="logo" src={logo} alt="Whiter logo" />
-                <div className="navigation__links">
-                    <ul>
-                        {links.map((link, idx) => {
-                            return (
+export class TopNavigation extends React.Component {
+    state = {
+        showMenu: false
+    };
+    toggleMenu = () => {
+        this.setState({
+            showMenu: !this.state.showMenu
+        });
+    };
+    openMenu = () => {
+        this.setState({
+            showMenu: true
+        });
+    };
+    closeMenu = () => {
+        this.setState({
+            showMenu: false
+        });
+    };
+
+    render() {
+        const menuClass = this.state.showMenu ? "show" : "";
+
+        return (
+            <header>
+                <nav className="navigation container">
+                    <img className="logo" src={logo} alt="Whiter logo" />
+                    <div className="navigation__links">
+                        <ul>
+                            {links.map((link, idx) => (
                                 <li key={idx}>
                                     <a href={link.link}>{link.text}</a>
                                 </li>
-                            );
-                        })}
-                    </ul>
-                </div>
+                            ))}
+                        </ul>
+                    </div>
 
-                <a className="navigation__action" href="">
-                    Sign in
-                </a>
-            </nav>
-            <img src="./images/menu 1.jpg" alt="" id="mobile-nav-icon" />
-            <nav className="mobile-nav">
-                <span className="close-icon">&times;</span>
-                {links.map((link, idx) => {
-                    return (
-                        <li key={idx}>
-                            <a href={link.link}>{link.text}</a>
-                        </li>
-                    );
-                })}
-            </nav>
-        </header>
-    );
+                    <a className="navigation__action" href="">
+                        Sign in
+                    </a>
+                </nav>
+                <img
+                    onClick={this.toggleMenu}
+                    src={burgerIcon}
+                    alt=""
+                    id="mobile-nav-icon"
+                />
+                {this.state.showMenu ? (
+                    <nav className={`mobile-nav ${menuClass}`}>
+                        <img
+                            className="mobile-nav-logo"
+                            src={logo}
+                            alt="Whiter logo"
+                        />
+                        <span className="close-icon" onClick={this.closeMenu}>
+                            &times;
+                        </span>
+                        <ul>
+                            {links.map((link, idx) => (
+                                <li key={idx}>
+                                    <a href={link.link}>{link.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                ) : null}
+            </header>
+        );
+    }
 }
+
+export default TopNavigation;
